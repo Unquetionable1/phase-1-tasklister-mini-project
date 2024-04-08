5cdbb25497cf7be54ec283ffb5ca9a5d2d2d0103
Edmound <eddnjoori@gmail.com>
2024-04-08 18:37:14 +0300

    Addx event listener to form

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const form=document.querySelector('form')
+  form.addEventListener('submit',(e)=>{
+    e.preventDefault()
+    listBuilder(e.target.new_task_description.value)
+    form.reset()
+  })
+
 });
+
+function listBuilder(todo){
+  console.log(todo);
+  const ul = document.querySelector('#tasks')
+  ul.innerHTML +=`<li>${todo}</li>`
+  console.log(todo);
+}
